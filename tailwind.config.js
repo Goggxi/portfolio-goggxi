c2bcdbef52e620abdb2cd171b0de8b158725eed3
goggxi <goggxi@gmail.com>
2022-04-01 19:30:38 +0800

    setup tailwind and create hero section

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px"
+    },
+    extend: {
+      colors: {
+        primary : "#f97316",
+        dark : "#171717"
+      },
+      screen: {
+        "2xl" : "1320px",
+      }
+    },
+  },
+  plugins: [],
+}
