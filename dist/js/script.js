94e1742469bcccfa7190682a457e0db921a910b1
goggxi <goggxi@gmail.com>
2022-04-12 13:52:12 +0800

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@
 window.onscroll = function () {
     const header = document.querySelector("header");
     const fixedNav = header.offsetTop;
-    
+
     if (window.pageYOffset > fixedNav) {
         header.classList.add("navbar-fixed")
     } else {
